b60c7b3b0377a8cdcbcdf7a2c08cfdf94b53bd82
Advaith H L <niceadvaith@gmail.com>
2023-09-12 10:01:05 +0530

    Add tf and workflows

---
+++ builders/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+data "aws_ami" "al2023" {
+  filter {
+    name   = "image-id"
+    values = ["ami-06f621d90fa29f6d0"]
+  }
+
+  owners = ["137112412989"] # amazon
+}
+
+data "aws_ec2_instance_type" "al2023" {
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "server1" {
+  ami           = data.aws_ami.al2023.id
+  instance_type = data.aws_ec2_instance_type.al2023.instance_type
+}
