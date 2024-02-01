98d6e7a4292fdfe3fea0b95c5ced0a5de683296c
Advaith H L <niceadvaith@gmail.com>
2024-02-01 16:43:20 +0530

    Remove data source as instance doesn't exist

--- builders/infra/main.tf
+++ builders/infra/main.tf
@@ -7,19 +7,4 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "ap-south-1"
-}
-
-data "aws_ami" "al2024" {
-  filter {
-    name   = "image-id"
-    values = ["ami-0866a04d72a1f5479"]
-  }
-
-  owners = ["137112412989"] # amazon
-}
-
-data "aws_ec2_instance_type" "al2024" {
-  instance_type = "t2.micro"
-}
+provider "aws" {}
