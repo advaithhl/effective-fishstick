f22ccee2751994324358fc0ee85c50dd97d0ba30
Advaith H L <niceadvaith@gmail.com>
2024-02-01 16:13:45 +0530

    Update AMI ID

--- builders/infra/main.tf
+++ builders/infra/main.tf
@@ -11,15 +11,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_ami" "al2023" {
+data "aws_ami" "al2024" {
   filter {
     name   = "image-id"
-    values = ["ami-06f621d90fa29f6d0"]
+    values = ["ami-0866a04d72a1f5479"]
   }
 
   owners = ["137112412989"] # amazon
 }
 
-data "aws_ec2_instance_type" "al2023" {
+data "aws_ec2_instance_type" "al2024" {
   instance_type = "t2.micro"
 }
